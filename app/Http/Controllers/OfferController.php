3be2ce02c2ec83e33def05d36a2b5cc97c341003
Rosiliza Rioja <rrioja@yondu.com>
2017-04-22 18:14:11 +0800

    Offer API: post request and validation

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Offer;
 use App\Models\Status;
+use App\Models\Trip;
+use App\Models\Request as RequestModel;
+use DB;
 
 class OfferController extends Controller
 {
@@ -23,10 +26,45 @@ class OfferController extends Controller
     {
       $this->_validateRequest($request);
 
+      $uuid = $request->input('uuid');
+      $request_id = $request->input('request_id');
+      $travel_from = $request->input('travel_from');
+      $travel_to = $request->input('travel_to');
+      $open_id = Status::ofName('open')->id;
+      $offered_id = Status::ofName('offered')->id;
+
+      $requestModel = DB::select("SELECT * FROM requests r WHERE id = {$request_id} AND (r.status_id = {$open_id} OR r.status_id = {$offered_id})");
+      if (empty($requestModel)) {
+        return $this->error('Invalid Request', 404);
+      }
+
+      // Check if Trip exists
+      $trip = Trip::where('uuid', $uuid)->where('travel_from', $travel_from)->where('travel_to', $travel_to)->count();
+
+      if (! $trip) {
+        $trip = new Trip;
+        $trip->uuid = $uuid;
+        $trip->travel_from = $travel_from;
+        $trip->travel_to = $travel_to;
+        $trip->travel_date = $request->input('travel_date');
+        $trip->save();
+      } else {
+        $trip = Trip::where('uuid', $uuid)->where('travel_from', $travel_from)->where('travel_to', $travel_to)->get()->first();
+      }
+      
       $offer = new Offer;
-      $offer->fill($request->all());
+      $offer->uuid = $uuid;
+      $offer->request_id = $request_id;
+      $offer->trip_id = $trip->id;
+      $offer->currency = $request->input('currency');
+      $offer->reward = $request->input('reward');
+      $offer->delivery_date = $request->input('delivery_date');
       $offer->status_id = Status::ofName('active')->id;
       $offer->save();
+
+      $requestModel = RequestModel::find($request_id);
+      $requestModel->status_id = Status::OfName('offered')->id;
+      $requestModel->update();
 
       return $this->success('Success', 201);
     }
@@ -62,8 +100,11 @@ class OfferController extends Controller
       $rules = [
         'uuid' => 'required',
         'request_id' => 'required|integer',
-        'trip_id' => 'required|integer',
-        'reward' => 'required|numeric'
+        'currency' => 'required',
+        'reward' => 'required|numeric',
+        'travel_from' => 'required',
+        'travel_to' => 'required',
+        'travel_date' => 'required|date'
       ];
 
       $this->validate($request, $rules);
