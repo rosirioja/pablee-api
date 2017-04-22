1c2141537d1a314e3caac7adb8d1585a973deef1
Rosiliza Rioja <rrioja@yondu.com>
2017-04-22 13:46:54 +0800

    Functions for json response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,15 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+  public function success($message, $code){
+      return response()->json(['message' => $message], $code);
+  }
+
+  public function successData($data, $code){
+      return response()->json($data, $code);
+  }
+
+  public function error($error, $code){
+      return response()->json(['error' => $error], $code);
+  }
 }
